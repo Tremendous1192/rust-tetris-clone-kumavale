7af625b91f7f1eee76ee10a92f53e759057b19b6
Tremendous1192 <tremendous.duck@gmail.com>
2023-12-04 20:14:56 +0900

    Chapter 08 機能追加（後編）

---
+++ ch08/tetrust_section04_point/src/main.rs
@@ -0,0 +1,118 @@
+mod block;
+mod game;
+
+use game::*;
+use getch_rs::{Getch, Key};
+use std::sync::{Arc, Mutex};
+use std::{thread, time};
+
+fn main() {
+    let game = Arc::new(Mutex::new(Game::new()));
+
+    // 画面クリア
+    println!("\x1b[2J\x1b[H\x1b[?25l");
+    // フィールドを描画
+    draw(&game.lock().unwrap());
+
+    // 自然落下処理
+    {
+        let game = Arc::clone(&game);
+        let _ = thread::spawn(move || {
+            loop {
+                // 1秒間スリーブする
+                thread::sleep(time::Duration::from_millis(1000));
+                // 自然落下
+                let mut game = game.lock().unwrap();
+                let new_pos = Position {
+                    x: game.pos.x,
+                    y: game.pos.y + 1,
+                };
+                if !is_collision(&game.field, &new_pos, &game.block) {
+                    // posの座標を更新
+                    game.pos = new_pos;
+                } else {
+                    // ブロック落下後の処理
+                    if landing(&mut game).is_err() {
+                        // ブロックを生成できないならゲームオーバー
+                        gameover(&game);
+                        break;
+                    }
+                }
+                // フィールドを描画
+                draw(&game);
+            }
+        });
+    }
+
+    // キー入力処理
+    let g = Getch::new();
+    loop {
+        // `q`キーでループを抜ける
+        // キー入力待ち
+        match g.getch() {
+            Ok(Key::Up) => {
+                // ハードドロップ
+                let mut game = game.lock().unwrap();
+                hard_drop(&mut game);
+                if landing(&mut game).is_err() {
+                    // ブロックを生成できないならゲームオーバー
+                    gameover(&game);
+                    break;
+                }
+                draw(&game);
+            }
+            Ok(Key::Left) => {
+                let mut game = game.lock().unwrap();
+                let new_pos = Position {
+                    x: game.pos.x.checked_sub(1).unwrap_or(game.pos.x),
+                    y: game.pos.y,
+                };
+                move_block(&mut game, new_pos);
+                draw(&game);
+            }
+            Ok(Key::Down) => {
+                let mut game = game.lock().unwrap();
+                let new_pos = Position {
+                    x: game.pos.x,
+                    y: game.pos.y + 1,
+                };
+                move_block(&mut game, new_pos);
+                draw(&game);
+            }
+            Ok(Key::Right) => {
+                let mut game = game.lock().unwrap();
+                let new_pos = Position {
+                    x: game.pos.x + 1,
+                    y: game.pos.y,
+                };
+                move_block(&mut game, new_pos);
+                draw(&game);
+            }
+            Ok(Key::Char(' ')) => {
+                // ホールド
+                let mut game = game.lock().unwrap();
+                hold(&mut game);
+                draw(&game);
+            }
+            Ok(Key::Char('z')) => {
+                // 左回転
+                let mut game = game.lock().unwrap();
+                rotate_left(&mut game);
+                draw(&game);
+            }
+            Ok(Key::Char('x')) => {
+                // 右回転
+                let mut game = game.lock().unwrap();
+                rotate_right(&mut game);
+                draw(&game);
+            }
+            Ok(Key::Char('q')) => {
+                break;
+            }
+            _ => (), // 何もしない
+        }
+    }
+
+    // 終了処理
+    quit();
+}
